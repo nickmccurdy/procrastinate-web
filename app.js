ac6182f809d0a6eb5c24f7bed4b1dcbb89617b51
Nicolas McCurdy <thenickperson@gmail.com>
2015-09-18 14:34:16 -0400

    Minor reorganization of app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+var koa = require('koa');
 var logger = require('koa-logger');
 var serve = require('koa-static');
-var koa = require('koa');
+
 var app = koa();
 
 app.use(logger());
 app.use(serve('public'));
+
 app.listen(3000);
